d62d8565b400b31f3c62b3e942cd9fc01991197d
Dominux <kir.gimranov@gmail.com>
2023-05-08 00:54:50 +0300

    Removed dumb gamelib

--- game_lib/src/key_gens.rs
+++
@@ -1,6 +0,0 @@
-use crate::types::TileKey;
-
-/// Generate random tile key
-pub(crate) fn gen_random_tilekey() -> TileKey {
-    fastrand::u8(..)
-}
--- game_lib/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod game;
-pub mod key_gens;
-pub mod models;
-pub mod types;
-
-#[cfg(test)]
-mod tests;
