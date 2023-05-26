90fbcd32e17bdf7232df6f4145bbd4cdc2a7e32b
Kirill Gimranov <k.gimranov@vk.team>
2023-05-26 11:22:53 +0300

    set_interval and canvas rendering work just fine

--- src/components/Playground.rs
+++ src/components/Playground.rs
@@ -2,17 +2,15 @@ use std::f64;
 use std::time::Duration;
 
 use leptos::*;
-
-use crate::{components::TilesColumn::*, enums::CheckingResult};
+use wasm_bindgen::prelude::*;
+use web_sys::CanvasRenderingContext2d;
 
 #[component]
 pub fn Playground(cx: Scope) -> impl IntoView {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into().unwrap();
+    let (k, set_k) = create_signal(cx, 2.0);
 
     let context = canvas
         .get_context("2d")
@@ -20,42 +18,47 @@ pub fn Playground(cx: Scope) -> impl IntoView {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
-
-    let view = view! { cx, ()};
+    let view = view! { cx,
+        <h1 class="playground_header">{"Score"}</h1>
+    };
 
     let _ = set_interval_with_handle(
         move || {
-            for (_, set_is_add_row) in cols.iter() {
-                set_is_add_row.set(true)
-            }
+            draw_smile(&context, k());
+            set_k.update(|k| *k -= 0.01)
         },
-        Duration::new(3, 0),
+        Duration::from_millis(17), // ~ 60fps
     );
 
     view
+}
+
+fn draw_smile(ctx: &CanvasRenderingContext2d, k: f64) {
+    let pi_k = f64::consts::PI * k;
+
+    // clearing canvas
+    {
+        let canvas = ctx.canvas().unwrap();
+        canvas.set_width(canvas.width())
+    }
+
+    ctx.begin_path();
+
+    // Draw the outer circle.
+    ctx.arc(75.0, 75.0, 50.0, 0.0, pi_k).unwrap();
+
+    // Draw the mouth.
+    ctx.move_to(110.0, 75.0);
+    ctx.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI * k / 2.0)
+        .unwrap();
+
+    // Draw the left eye.
+    ctx.move_to(65.0, 65.0);
+    ctx.arc(60.0, 65.0, 5.0, 0.0, pi_k).unwrap();
+
+    // Draw the right eye.
+    ctx.move_to(95.0, 65.0);
+    ctx.arc(90.0, 65.0, 5.0, 0.0, pi_k).unwrap();
+
+    ctx.stroke();
 }
